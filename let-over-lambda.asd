3e13c0af315a8a0a819103aedb355e6366885fcc
Colin J.E. Lupton <colin.lupton@gmail.com>
2013-10-16 15:29:43 -0400

    Add let-over-lambda source + asdf system, cl package defs

---
+++ let-over-lambda.asd
@@ -0,0 +1,25 @@
+;;;; -*- Mode: LISP; Syntax: COMMON-LISP; Package: LET-OVER-LAMBDA; Base: 10 -*- file: let-over-lambda.asd
+
+(in-package :cl-user)
+
+(defpackage let-over-lambda-asd
+	(:use :cl :asdf))
+
+(in-package :let-over-lambda-asd)
+
+(defvar *lol-version* "1.0"
+	"A string denoting the current version of LET-OVER-LAMBDA.  Used for diagnostic output.")
+
+(export '*lol-version*)
+
+(defsystem #:let-over-lambda
+  :serial t
+  :description "The Production version code from letoverlambda.com, conveniently wrapped in an ASDF System for Quicklisp."
+  :version #.*lol-version*
+  :author "Doug Hoyte"
+  :license "Attribution"
+  :depends-on (#:cl-ppcre)
+  :components ((:file "package")
+               (:file "let-over-lambda")))
+
+;; EOF
---
+++ package.lisp
@@ -0,0 +1,56 @@
+;;;; -*- Mode: LISP; Syntax: COMMON-LISP; Package: LET-OVER-LAMBDA; Base: 10 -*- file: package.lisp
+
+(defpackage #:let-over-lambda
+  (:nicknames #:lol)
+  (:use #:cl #:cl-user #:cl-ppcre)
+  (:export #:mkstr
+           #:symb
+           #:group
+           #:flatten
+           #:fact
+           #:choose
+           #:g!-symbol-p
+           #:defmacro/g!
+           #:o!-symbol-p
+           #:o!-symbol-to-g!-symbol
+           #:defmacro!
+           #:|#"-reader|
+           #:segment-reader
+           #:match-mode-ppcre-lambda-form
+           #:subst-mode-ppcre-lambda-form
+           #:|#~-reader|
+           #:dlambda
+           #:alambda
+           #:aif
+           #:|#`-reader|
+           #:alet%
+           #:alet
+           #:let-binding-transform
+           #:pandoriclet
+           #:pandoriclet-get
+           #:pandoriclet-set
+           #:get-pandoric
+           #:with-pandoric
+           #:pandoric-hotpatch
+           #:pandoric-recode
+           #:plambda
+           #:pandoric-eval
+           #:fast-progn
+           #:safe-progn
+           #:fformat
+           #:make-tlist
+           #:tlist-left
+           #:tlist-right
+           #:tlist-empty-p
+           #:tlist-add-left
+           #:tlist-add-right
+           #:tlist-rem-left
+           #:tlist-update
+           #:build-batcher-sn
+           #:sortf
+           #:dollar-symbol-p
+           #:prune-if-match-bodies-from-sub-lexical-scope
+           #:if-match
+           #:when-match))
+
+;; EOF
